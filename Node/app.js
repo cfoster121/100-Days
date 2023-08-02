1bdfc344858736906b09b67b29a6e8ad8e61189d
cfoster121 <cfoster649@gmail.com>
2023-08-02 16:33:21 -0700

    NodeJS + ExpressJS - Day 48/49

--- Node/app.js
+++ Node/app.js
@@ -1,21 +1,48 @@
-// let name = "Courtney"
+const fs = require('fs')
+const path = require('path')
+const express = require('express');
 
-// console.log(name)
+const app = express()
 
+app.listen(3000)
 
-const http = require('http')
+app.use(express.urlencoded({extended: false}))
 
-function handleRequest(req, res){
-    if(req.url === '/currenttime') {
-        res.statusCode = 200;
-        res.end('<h1>' + new Date().toISOString() + '</h1>')    
+app.get('/currenttime', function(req, res){
+    res.send('<h1>' + new Date().toISOString() + '</h1>');   
+})
+
+app.get('/', function(req,res) {
+    res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>')    
+})
+
+app.post('/store-user', function(req, res) {
+    const userName = req.body.username
+
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    existingUsers.push(userName)
+
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers))
+
+    res.send('<h1>Username stored</h1>')
+
+app.get('/users', function(req, res) {
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    let responseData = '<ul>';
+
+    for (const user of existingUsers) {
+        responseData += '<li>' + user + '</li>'
     }
-    else if(req.url === '/') {
-        res.statusCode = 200;
-        res.end('<h1>hello</h1>')    
-    }
-}
 
-const server = http.createServer(handleRequest)
+    responseData += '</ul>';
 
-server.listen(3000);
+    res.send(responseData);
+
+})
+})
