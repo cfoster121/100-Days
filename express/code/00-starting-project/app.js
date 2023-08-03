1c34a744aad1aaed03612fc0c7edd938534f86de
cfoster121 <cfoster649@gmail.com>
2023-08-03 14:26:13 -0700

    Dynamic/Static ejs - day 51

--- express/code/00-starting-project/app.js
+++ express/code/00-starting-project/app.js
@@ -1,35 +1,57 @@
 const express = require('express')
 const path = require('path')
+const fs = require ('fs')
 
-const app = express()
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-    res.send('<h1>testing app</h1>')
+    res.render('index')
 })
 
 app.get('/restaurants', function(req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html')
-    res.sendFile(htmlFilePath)
+    
+    const filePath = path.join(__dirname, "data", "restaurants.json")
+
+    const fileData = fs.readFileSync(filePath)
+    const storedRestaurants = JSON.parse(fileData)
+
+    res.render('restaurants', {
+        numberOfRestaurants: storedRestaurants.length,
+        restaurants: storedRestaurants 
+    })
 })
 
 app.get('/recommend', function(req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html')
-    res.sendFile(htmlFilePath)
+    res.render('recommend')
 })
 
-app.get('/index', function(req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'index.html')
-    res.sendFile(htmlFilePath)
+app.post('/recommend', (req, res) => {
+    res.render('recommend')
+    
+    const filePath = path.join(__dirname, "data", "restaurants.json")
+
+    const fileData = fs.readFileSync(filePath)
+    const storedRestaurants = JSON.parse(fileData)
+
+    storedRestaurants.push(restaurant)
+
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    res.redirect('/confirm');
 })
 
 app.get('/confirm', function(req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html')
-    res.sendFile(htmlFilePath)
+    res.render('confirm')
 })
 
 app.get('/about', function(req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'about.html')
-    res.sendFile(htmlFilePath)
+    res.render('about')
 })
 
 
